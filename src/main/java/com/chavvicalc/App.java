47896fc0d7a039072e7b14cf401ac40cbd9f68df
MuhammadKhalid0 <74385591+MuhammadKhalid0@users.noreply.github.com>
2024-09-25 03:18:51 +0300

    Initial commit

---
+++ src/main/java/com/chavvicalc/App.java
@@ -0,0 +1,13 @@
+package com.chavvicalc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
